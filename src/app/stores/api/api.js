4de8ccfae0453a5b1a8a9b6ceccddc69fcf25ca4
AMStrix <aaron.m.stricker@gmail.com>
2015-06-30 07:41:40 -0500

    minor api mods

--- src/app/stores/api/api.js
+++ src/app/stores/api/api.js
@@ -3,48 +3,43 @@ var $ = require('jquery');
 var _ = require('underscore');
 var Reflux = require('reflux');
 
-var Authentication = require('./authentication.js');
-
 var actions = Reflux.createActions([
-	// in
-	'initialize',
+    // in
+    'initialize',
 
-	// out
-	'apiInitialized',
-	'apiAjaxFail'
+    // out
+    'apiInitialized',
+    'apiAjaxFail'
 ]);
 
 function url(service){
-	return _config_.url+'/'+service;
+    return _config_.url+'/'+service;
 }
 
 var Api = {
-	ajax: function(service, data) {
-		return $.ajax({
-			url: url(service),
-			dataType: 'json',
-			contentType: 'application/json',
-			type: 'POST',
-			data: JSON.stringify(data||{})
-		}).done(function(data, textStatus, jqXHR) {
-			// console.log(data);
-			// console.log(textStatus);
-			// console.log(jqXHR);
-			actions.apiInitialized();
-			require('../main.js').actions.clearApiAjaxError();
-		}).fail(function(jqXHR, textStatus, errorThrown) {
-			actions.apiAjaxFail(
-				_.extend({}, jqXHR.responseJSON, {
-					status: jqXHR.status,
-					text: jqXHR.statusText
-				})
-			);
-		});
-	}
+    ajax: function(service, data) {
+        $.support.cors = true;
+        return $.ajax({
+            url: url(service),
+            dataType: 'json',
+            contentType: 'application/json',
+            type: 'POST',
+            data: JSON.stringify(data||{})
+        }).done(function(data, textStatus, jqXHR) {
+            actions.apiInitialized();
+            require('../main.js').actions.clearApiAjaxError();
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            actions.apiAjaxFail(
+                _.extend({}, jqXHR.responseJSON, {
+                    status: jqXHR.status,
+                    text: jqXHR.statusText
+                })
+            );
+        });
+    }
 };
 
 
 
-module.exports = {api: Api, actions: actions};
+module.exports = {api: Api, actions: actions}; 
 
-// courtc.https://projectbpprd.performnet.com/projectb/api/index.html/buzzer
